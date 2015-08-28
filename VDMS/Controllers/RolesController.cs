c9b170a26772d41fe53c4e6c91b37fe7c4bcaf05
MaximT <maxim.tabacari@vodafone.com>
2015-08-28 16:04:25 +0300

    Roles CRUD

---
+++ VDMS/Controllers/RolesController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using VDMS.Models;
+using System.Net;
+
+namespace VDMS.Controllers
+{
+    public class RolesController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        // GET: Roles
+        public ActionResult Index()
+        {
+            var myrole = context.Roles.ToList();
+            return View(myrole);
+        }
+
+        // GET: Roles/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Roles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Roles/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {                
+                context.Roles.Add(new IdentityRole()
+                {
+                    Name = collection["RoleName"]
+                });
+                context.SaveChanges();
+                ViewBag.ResultMessage = "Role created successfully!";       
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Roles/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IdentityRole myrole = context.Roles.Find(id);
+            if (myrole == null)
+            {
+                return HttpNotFound();
+            }
+            return View(myrole);
+        }
+
+        // POST: Roles/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Microsoft.AspNet.Identity.EntityFramework.IdentityRole role)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    context.Entry(role).State = System.Data.Entity.EntityState.Modified;
+                    context.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Roles/Delete/5
+        public ActionResult Delete(string id)
+        {
+            var myrole = context.Roles.Find(id);            
+            return View(myrole);
+        }
+
+        // POST: Roles/Delete/5
+        [HttpPost]
+        public ActionResult Delete(string id, FormCollection collection)
+        {
+            try
+            {
+                var myrole = context.Roles.Find(id);
+                context.Roles.Remove(myrole);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
