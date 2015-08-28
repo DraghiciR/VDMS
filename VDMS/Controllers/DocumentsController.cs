6861634fa933d368978cb5c33e4f266415f5f1b9
MaximT <maxim.tabacari@vodafone.com>
2015-08-28 12:27:21 +0300

    Scaffolding for Branches, DocumentTypes and Documents

---
+++ VDMS/Controllers/DocumentsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using VDMS.Models;
+
+namespace VDMS.Controllers
+{
+    public class DocumentsController : Controller
+    {
+        private VDMSModel db = new VDMSModel();
+
+        // GET: Documents
+        public ActionResult Index()
+        {
+            var documents = db.Documents.Include(d => d.Branch).Include(d => d.DocumentType);
+            return View(documents.ToList());
+        }
+
+        // GET: Documents/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Document document = db.Documents.Find(id);
+            if (document == null)
+            {
+                return HttpNotFound();
+            }
+            return View(document);
+        }
+
+        // GET: Documents/Create
+        public ActionResult Create()
+        {
+            ViewBag.BranchID = new SelectList(db.Branches, "BranchID", "Name");
+            ViewBag.DocTypeID = new SelectList(db.DocumentTypes, "DocTypeID", "Name");
+            return View();
+        }
+
+        // POST: Documents/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DocID,DocSerial,DocTypeID,BranchID,UserID,Inbound,Recipient,Description,CreationDate")] Document document)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Documents.Add(document);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BranchID = new SelectList(db.Branches, "BranchID", "Name", document.BranchID);
+            ViewBag.DocTypeID = new SelectList(db.DocumentTypes, "DocTypeID", "Name", document.DocTypeID);
+            return View(document);
+        }
+
+        // GET: Documents/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Document document = db.Documents.Find(id);
+            if (document == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BranchID = new SelectList(db.Branches, "BranchID", "Name", document.BranchID);
+            ViewBag.DocTypeID = new SelectList(db.DocumentTypes, "DocTypeID", "Name", document.DocTypeID);
+            return View(document);
+        }
+
+        // POST: Documents/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DocID,DocSerial,DocTypeID,BranchID,UserID,Inbound,Recipient,Description,CreationDate")] Document document)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(document).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BranchID = new SelectList(db.Branches, "BranchID", "Name", document.BranchID);
+            ViewBag.DocTypeID = new SelectList(db.DocumentTypes, "DocTypeID", "Name", document.DocTypeID);
+            return View(document);
+        }
+
+        // GET: Documents/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Document document = db.Documents.Find(id);
+            if (document == null)
+            {
+                return HttpNotFound();
+            }
+            return View(document);
+        }
+
+        // POST: Documents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Document document = db.Documents.Find(id);
+            db.Documents.Remove(document);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
