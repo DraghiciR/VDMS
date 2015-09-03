6c3adf5a83afbf829f6fdb6f170830a56a261663
andreidanavdf <andrei.dana@vodafone.com>
2015-09-03 16:30:36 +0300

    Enable exception logging using web.config variable

--- VDMS/Models/Helpers/ExceptionLogger.cs
+++ VDMS/Models/Helpers/ExceptionLogger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -21,39 +22,44 @@ namespace VDMS.Models.Helpers
         
         public static void LogException(Exception exc, string source)
         {
-            // Include logic for logging exceptions
-            // Get the absolute path to the log file
-            string logFile = "../App_Data/VdmsLog.txt";
-            logFile = HttpContext.Current.Server.MapPath(logFile);
+            Boolean isLoggingEnabled = Convert.ToBoolean(ConfigurationManager.AppSettings["EnableExceptionLogging"].ToString());
 
-            // Open the log file for append and write the log
-            StreamWriter sw = new StreamWriter(logFile, true);
-            sw.WriteLine("********** {0} **********", DateTime.Now);
-            if (exc.InnerException != null)
+            if (isLoggingEnabled == true)
             {
-                sw.Write("Inner Exception Type: ");
-                sw.WriteLine(exc.InnerException.GetType().ToString());
-                sw.Write("Inner Exception: ");
-                sw.WriteLine(exc.InnerException.Message);
-                sw.Write("Inner Source: ");
-                sw.WriteLine(exc.InnerException.Source);
-                if (exc.InnerException.StackTrace != null)
+                // Include logic for logging exceptions
+                // Get the absolute path to the log file
+                string logFile = "../App_Data/VdmsLog.txt";
+                logFile = HttpContext.Current.Server.MapPath(logFile);
+
+                // Open the log file for append and write the log
+                StreamWriter sw = new StreamWriter(logFile, true);
+                sw.WriteLine("********** {0} **********", DateTime.Now);
+                if (exc.InnerException != null)
                 {
-                    sw.WriteLine("Inner Stack Trace: ");
-                    sw.WriteLine(exc.InnerException.StackTrace);
+                    sw.Write("Inner Exception Type: ");
+                    sw.WriteLine(exc.InnerException.GetType().ToString());
+                    sw.Write("Inner Exception: ");
+                    sw.WriteLine(exc.InnerException.Message);
+                    sw.Write("Inner Source: ");
+                    sw.WriteLine(exc.InnerException.Source);
+                    if (exc.InnerException.StackTrace != null)
+                    {
+                        sw.WriteLine("Inner Stack Trace: ");
+                        sw.WriteLine(exc.InnerException.StackTrace);
+                    }
                 }
+                sw.Write("Exception Type: ");
+                sw.WriteLine(exc.GetType().ToString());
+                sw.WriteLine("Exception: " + exc.Message);
+                sw.WriteLine("Source: " + source);
+                sw.WriteLine("Stack Trace: ");
+                if (exc.StackTrace != null)
+                {
+                    sw.WriteLine(exc.StackTrace);
+                    sw.WriteLine();
+                }
+                sw.Close();
             }
-            sw.Write("Exception Type: ");
-            sw.WriteLine(exc.GetType().ToString());
-            sw.WriteLine("Exception: " + exc.Message);
-            sw.WriteLine("Source: " + source);
-            sw.WriteLine("Stack Trace: ");
-            if (exc.StackTrace != null)
-            {
-                sw.WriteLine(exc.StackTrace);
-                sw.WriteLine();
-            }
-            sw.Close();
         }
     }
 }
