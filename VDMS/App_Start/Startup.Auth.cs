75bd6c13ee1fa756c5cdf2efb9aab9dd1a940fe3
CristinaR <anacristina.radulescu@vodafone.com>
2015-09-16 12:04:52 +0300

    Session ends in 30 minutes, updated security stamp when user is disabled in order to require re-login, reduced security stamp validate interval to 1 second in order to require immediate re-login for disabled users or users with changed roles

--- VDMS/App_Start/Startup.Auth.cs
+++ VDMS/App_Start/Startup.Auth.cs
@@ -24,6 +24,7 @@ namespace VDMS
             // Configure the sign in cookie
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
+                ExpireTimeSpan = TimeSpan.FromMinutes(30),
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString("/Account/Login"),
                 Provider = new CookieAuthenticationProvider
@@ -31,7 +32,7 @@ namespace VDMS
                     // Enables the application to validate the security stamp when the user logs in.
                     // This is a security feature which is used when you change a password or add an external login to your account.  
                     OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
-                        validateInterval: TimeSpan.FromMinutes(30),
+                        validateInterval: TimeSpan.FromSeconds(1),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
             });            
